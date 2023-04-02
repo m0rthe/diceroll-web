3d066566504f1e60a2989a7e6028f57f15060903
Moritz Theodoridis <moritz.theodoridis@googlemail.com>
2023-04-02 16:25:00 +0200

    Add files via upload

---
+++ scripts/diceroll.js
@@ -0,0 +1,39 @@
+//function for testing
+function test() {
+    alert("test"); //edit if needed
+}
+
+//function that rolls a d4 (number range: 1 to 4)
+function d4() {
+   alert(`Rolling your dice ... ${Math.floor(1 + Math.random() * 4)}`);
+}
+
+//function that rolls a d6 (number range: 1 to 6)
+function d6() {
+    alert(`Rolling your dice ... ${Math.floor(1 + Math.random() * 6)}`);
+ }
+
+ //function that rolls a d8 (number range: 1 to 8)
+ function d8() {
+    alert(`Rolling your dice ... ${Math.floor(1 + Math.random() * 8)}`);
+ }
+
+ //function that rolls a d10 (number range: 1 to 10)
+ function d10() {
+    alert(`Rolling your dice ... ${Math.floor(1 + Math.random() * 10)}`);
+ }
+
+ //function that rolls a d12 (number range: 1 to 12)
+ function d12() {
+    alert(`Rolling your dice ... ${Math.floor(1 + Math.random() * 12)}`);
+ }
+
+ //function that rolls a d20 (number range: 1 to 20)
+ function d20() {
+    alert(`Rolling your dice ... ${Math.floor(1 + Math.random() * 20)}`);
+ }
+
+ //function that rolls a d100 (number range: 1 to 100)
+ function d100() {
+    alert(`Rolling your dice ... ${Math.floor(1 + Math.random() * 100)}`);
+ }
